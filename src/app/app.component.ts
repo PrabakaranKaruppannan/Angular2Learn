9e8b5ec645fb68867421283aab6409db1b47096c
Prabakaran, Karuppannan (GfK) <Karuppannan.Prabakaran@gfk.com>
2017-07-03 11:46:00 +0800

    Initial Version Learning Angular 2 with Tour Of Heroes tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service'
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) {
+
+  }
+
+  getHeroes() {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  selectedHeroChanged(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
+
+
+
---
+++ src/app/heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const Heroes: Hero[] = [
+  {id: 1, name: 'Iron Man'},
+  {id: 2, name: 'Bat Man'},
+  {id: 3, name: 'Super Man'},
+  {id: 4, name: 'Captain America'},
+  {id: 5, name: 'Black Panther'},
+  {id: 6, name: 'Ant Man'},
+  {id: 7, name: 'Flash'},
+  {id: 8, name: 'Super Woman'},
+  {id: 9, name: 'Black Widow'},
+  {id: 10, name: 'Thor'}
+];
